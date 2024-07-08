912c009a446d8e906e6bf7adf5b6a5f4227f1d53
George Leonard <georgelza@gmail.com>
2024-07-08 17:22:42 +0200

    Fixing Flink queries/bits

--- Flink/creFlink_2.sql
+++ Flink/creFlink_2.sql
@@ -61,10 +61,6 @@ CREATE TABLE avro_salespayments (
 -- https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/connectors/table/formats/avro-confluent/
 -- Our avro_salescompleted output table which will push values to the Kafka topic.
 CREATE TABLE avro_salescompleted_x (
-    -- one column mapped to the Kafka raw UTF-8 key
-    the_kafka_key STRING,
-
-      -- a few columns mapped to the Avro fields of the Kafka value
     INVOICENUMBER STRING,
     SALEDATETIME STRING,
     SALETIMESTAMP STRING,
@@ -98,7 +94,6 @@ CREATE TABLE avro_salescompleted_x (
 -- Join 2 tables into avro_salescompleted_x - completed sales.
 Insert into avro_salescompleted_x
     SELECT 
-        b.INVOICENUMBER as the_kafka_key,
         b.INVOICENUMBER,
         a.SALEDATETIME,
         a.SALETIMESTAMP,
@@ -118,7 +113,6 @@ Insert into avro_salescompleted_x
         avro_salespayments b
     ON a.INVOICENUMBER = b.INVOICENUMBER;
 
-
 -- Create sales per store per terminal per hour output table
 
 
@@ -129,3 +123,64 @@ Insert into avro_salescompleted_x
 
 
 -- Calculate sales per store per terminal per 5 min - dev purposes
+
+
+
+
+
+-- OLD
+CREATE TABLE avro_salescompleted_x (
+    -- one column mapped to the Kafka raw UTF-8 key
+    the_kafka_key STRING,
+
+      -- a few columns mapped to the Avro fields of the Kafka value
+    INVOICENUMBER STRING,
+    SALEDATETIME STRING,
+    SALETIMESTAMP STRING,
+    TERMINALPOINT STRING,
+    NETT DOUBLE,
+    VAT DOUBLE,
+    TOTAL DOUBLE,
+    STORE row<ID STRING, NAME STRING>,
+    CLERK row<ID STRING, NAME STRING>,
+    BASKETITEMS array<row<ID STRING, NAME STRING, BRAND STRING, CATEGORY STRING, PRICE DOUBLE, QUANTITY INT>>,
+    FINTRANSACTIONID STRING,
+    PAYDATETIME STRING,
+    PAYTIMESTAMP STRING,
+    PAID DOUBLE,
+    SALESTIMESTAMP_WM AS TO_TIMESTAMP(FROM_UNIXTIME(CAST(SALETIMESTAMP AS BIGINT) / 1000)),
+    PAYTIMESTAMP_WM AS TO_TIMESTAMP(FROM_UNIXTIME(CAST(PAYTIMESTAMP AS BIGINT) / 1000)),
+    WATERMARK FOR SALESTIMESTAMP_WM AS SALESTIMESTAMP_WM,
+    PRIMARY KEY (INVOICENUMBER) NOT ENFORCED
+) WITH (
+        'connector' = 'upsert-kafka',
+        'topic' = 'avro_salescompleted_x',
+        'properties.bootstrap.servers' = 'broker:29092',
+        'key.format' = 'raw',
+        'properties.group.id' = 'testGroup',
+        'value.format' = 'avro-confluent',
+        'value.avro-confluent.url' = 'http://schema-registry:8081',
+        'value.fields-include' = 'ALL'
+);
+
+Insert into avro_salescompleted_x
+    SELECT 
+        b.INVOICENUMBER as the_kafka_key,
+        b.INVOICENUMBER,
+        a.SALEDATETIME,
+        a.SALETIMESTAMP,
+        a.TERMINALPOINT,
+        a.NETT,
+        a.VAT,
+        a.TOTAL,
+        a.STORE,
+        a.CLERK,
+        a.BASKETITEMS,        
+        b.FINTRANSACTIONID,
+        b.PAYDATETIME,
+        b.PAYTIMESTAMP,
+        b.PAID
+    FROM 
+        avro_salesbaskets a LEFT JOIN 
+        avro_salespayments b
+    ON a.INVOICENUMBER = b.INVOICENUMBER;
